472abf9a475e578e894346b4a239f1a2b2e6f3a3
Luís Teixeira <micboucinha@gmail.com>
2020-08-07 11:32:55 +0100

    fix ENABLE_MOTOR_DRIVER register handling

--- Firmware/Pump/app_funcs.c
+++ Firmware/Pump/app_funcs.c
@@ -54,24 +54,17 @@ bool (*app_func_wr_pointer[])(void*) = {
 /************************************************************************/
 /* REG_ENABLE_MOTOR_DRIVER                                              */
 /************************************************************************/
-uint16_t reset_motor_traveling;
-
 void app_read_REG_ENABLE_MOTOR_DRIVER(void){}
 bool app_write_REG_ENABLE_MOTOR_DRIVER(void *a)
 {
 	uint8_t reg = *((uint8_t*)a);
 	
-	if(reg & ~B_MOTOR_ENABLE)
-		return false;
-		
-	/* if the motor is not enabled, check if we want to enable it */
-	if(app_regs.REG_ENABLE_MOTOR_DRIVER == ~B_MOTOR_ENABLE)
-	{
-		if(reg == B_MOTOR_ENABLE)
-		{
-			app_regs.REG_ENABLE_MOTOR_DRIVER = B_MOTOR_ENABLE;
-		}
-	}
+	if(reg)
+		set_EN_DRIVER;
+	else
+		clr_EN_DRIVER;
+	
+	app_regs.REG_ENABLE_MOTOR_DRIVER = reg;
 
 	return true;
 }
