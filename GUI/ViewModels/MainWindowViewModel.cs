f32db2919b23705a983437bd49846cdeebf2dcab
Luís Teixeira <micboucinha@gmail.com>
2020-11-17 18:04:35 +0000

    [gui] startup project initial commit

---
+++ GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Device.Pump.GUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
