faf8ed5f58da99ff7a6f02701561c18258dabb55
Luís Teixeira <micboucinha@gmail.com>
2020-11-23 17:59:44 +0000

    [gui] add COM port detection

--- GUI/ViewModels/SyringePumpViewModel.cs
+++ GUI/ViewModels/SyringePumpViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Reactive;
 using System.Text;
-using System.Threading.Tasks;
+using Bonsai.Harp;
+using ReactiveUI;
 
 namespace Device.Pump.GUI.ViewModels
 {
@@ -10,9 +13,30 @@ namespace Device.Pump.GUI.ViewModels
     {
         public string AppVersion { get; set; }
 
+        private List<string> _ports = new List<string>();
+
+        public List<string> Ports
+        {
+            get => _ports;
+            set => this.RaiseAndSetIfChanged(ref _ports, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> LoadDeviceInformation { get; }
+
         public SyringePumpViewModel()
         {
             AppVersion = "v"+ typeof(SyringePumpViewModel).Assembly.GetName().Version?.ToString(3);
+
+            LoadDeviceInformation = ReactiveCommand.Create(LoadUSBInformation);
+
+            // force initial population of currently connected ports
+            LoadUSBInformation();
+        }
+
+        public void LoadUSBInformation()
+        {
+            var devices = SerialPort.GetPortNames();
+            Ports = devices?.ToList();
         }
     }
 }
