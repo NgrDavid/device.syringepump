78101385b35ec277afbb109d478f07115fe50d03
Luís Teixeira <micboucinha@gmail.com>
2020-12-16 09:54:23 +0000

    [gui] add validation to Configuration fields

--- GUI/ViewModels/SyringePumpViewModel.cs
+++ GUI/ViewModels/SyringePumpViewModel.cs
@@ -2,27 +2,47 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Text;
+using System.Reactive.Subjects;
+using System.Threading;
 using Bonsai.Harp;
-using Device.Pump.GUI.Models;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 
 #endregion
 
 namespace Device.Pump.GUI.ViewModels
 {
-    public class SyringePumpViewModel : ViewModelBase
+    public class SyringePumpViewModel : ReactiveValidationObject
     {
         public string AppVersion { get; set; }
 
         [Reactive] public List<string> Ports { get; set; }
 
         [Reactive] public string SelectedPort { get; set; }
+
+        [Reactive] public bool StepStateEvent { get; set; } = true;
+        [Reactive] public bool DirectionStateEvent { get; set; } = true;
+        [Reactive] public bool SwitchForwardStateEvent { get; set; } = true;
+        [Reactive] public bool SwitchReverseStateEvent { get; set; } = true;
+        [Reactive] public bool InputStateEvent { get; set; } = true;
+
+        [Reactive] public int ProtocolType { get; set; } = 0;
+        
+        [Reactive] public int NumberOfSteps { get; set; } = 15;
+        [Reactive] public int StepPeriod { get; set; } = 10;
+        [Reactive] public float Flowrate { get; set; } = 0.5f;
+        [Reactive] public float Volume { get; set; } = 0.5f;
+        [Reactive] public int MotorMicrostep { get; set; } = 0;
+        [Reactive] public int DigitalInput0Config { get; set; } = 0;
+        [Reactive] public int DigitalOutput0Config { get; set; } = 0;
+        [Reactive] public int DigitalOutput1Config { get; set; } = 0;
 
         public ReactiveCommand<Unit, Unit> LoadDeviceInformation { get; }
         public ReactiveCommand<string, Unit> ConnectAndGetBaseInfoCommand{ get; }
@@ -38,6 +58,20 @@ namespace Device.Pump.GUI.ViewModels
 
             ConnectAndGetBaseInfoCommand = ReactiveCommand.Create<string>(ConnectAndGetBaseInfo, canConnect);
             ConnectAndGetBaseInfoCommand.ThrownExceptions.Subscribe(ex => Console.WriteLine(ex.Message));
+
+            // Validation rules
+            this.ValidationRule(viewModel => viewModel.NumberOfSteps,
+                steps => steps > 0 && steps < 65535,
+                "Number of steps only accepts integer values between 1 and 65535");
+            this.ValidationRule(viewModel => viewModel.StepPeriod,
+                period => period > 0 && period < 65535,
+                "Step Period only accepts integer values between 1 and 65535");
+            this.ValidationRule(viewModel => viewModel.Flowrate,
+                flowRate => flowRate >= 0.5f && flowRate <= 2000f,
+                "Flowrate only accepts float values between 0.5 and 2000");
+            this.ValidationRule(viewModel => viewModel.Volume,
+                volume => volume >= 0.5f && volume <= 2000f,
+                "Volume only accepts float values between 0.5 and 2000");
 
             // force initial population of currently connected ports
             LoadUSBInformation();
